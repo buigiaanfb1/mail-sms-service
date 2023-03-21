10cbcd857e4c3f301f44ac9e4dd05df74dc5425e
Gia An <buigiaanfb1@gmail.com>
2023-03-21 23:24:56 +0700

    feat: send sms

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
+import twilio from "twilio";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const accountSid = "AC22d38bf92999df8ced96a0c869b5d027";
+const authToken = "2dc8c97a68b8890cb4a6fbba7232be17";
+const client = twilio(accountSid, authToken);
+
 app.post("/send-email", async (req: express.Request, res: express.Response) => {
-  console.log("1");
   const { to, subject, text } = req.body;
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -31,6 +35,22 @@ app.post("/send-email", async (req: express.Request, res: express.Response) => {
   } catch (error) {
     res.status(500).send(error);
   }
+});
+
+app.post("/send-sms", async (req: express.Request, res: express.Response) => {
+  const { to, body } = req.body;
+  client.messages
+    .create({
+      body: body,
+      to: to,
+      from: "+15177900837",
+    })
+    .then((message) => {
+      res.status(200).send(`SMS message sent to ${message.to}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 app.listen(3000, () => {
