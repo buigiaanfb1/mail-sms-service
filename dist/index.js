71db9350906b8709eec75752d5e698716bad39af
Gia An <buigiaanfb1@gmail.com>
2023-08-26 13:41:19 +0700

    SVCW

--- dist/index.js
+++ dist/index.js
@@ -58,21 +58,18 @@ const emailTemplate = (content, footerName) => {
                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
                     <tr style="background: linear-gradient(to right, #7635dc, #01AB55); border: 8px">
                         <td style="padding: 20px; color: #fff; text-align: center;">
-                            <h1 style="margin: 0;">Alumni Platform</h1>
-                            <p style="margin: 10px 0 0;">Nền tảng kết nối cựu học sinh THPT</p>
+                            <h1 style="margin: 0;">Student Volunteer Connection</h1>
+                            <p style="margin: 10px 0 0;">Nền tảng kết nối sinh viên với các hoạt động tình nguyện</p>
                         </td>
                     </tr>
                     <tr>
                         <td style="padding: 20px; background-color: #fff; color: #000; font-size: 14px;">
                             ${content}
-                            <br/>
-                            <p style="margin: 0; color: #000">Xin cảm ơn bạn,</p>
-                            <p style="margin: 0.25rem 0 0 0; font-size: 14px; color: #000">${footerName ? footerName : "Alumni Team"}</p>
                         </td>
                     </tr>
                     <tr style="background: #000; color: #fff">
                         <td style="padding: 5px; text-align: center; font-size: 12px">
-                            <p style="padding: 0.75rem 0;">ALUMNI PLATFORM &copy; {{ year }}</p>
+                            <p style="padding: 0.75rem 0;">SVCW &copy; {{ year }}</p>
                         </td>
                     </tr>
                 </table>
